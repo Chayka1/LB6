1a353fd0ed51f657b846a87def25ebd63e00fef8
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2022-10-31 19:19:30 +0200

    LB6

---
+++ LB6_1/LB6_1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> mylist = new MyList<int>(5);
+            mylist[0] = 1;
+            Console.WriteLine("Емкость списка: {0} элемент(-ов)", mylist.Capacity);
+            Console.WriteLine("Список фактически содержит: {0} элемент(-ов)", mylist.Count);
+            mylist.Add(15);
+            Console.WriteLine("Емкость списка: {0} элемент(-ов)", mylist.Capacity);
+ 
+            Console.ReadKey();
+        }
+    }
+}
